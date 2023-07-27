53ee14b730fddddd4fb6bec933ea84bc68f43d31
LaZoark <emersonchen086@icloud.com>
2023-07-27 15:53:57 +0800

    optimize cli rule

--- input_test.py
+++ input_test.py
@@ -9,36 +9,20 @@ def main():
   cli_history: list = []
   hist_iter_flag: int = 0
   _lock_cli: bool = True
-  go = True
+  go: bool = True
   logging.info("Typing 'stop' to terminate the server.")
 
   while go:
     _input_str: str = ""
     while True:
       c = sys.stdin.read(1)
-      
-      if c == 'u':
-        pass
-        # _lock_cli = False
-        # hist_iter_flag += 1
-        # if len(cli_history)>=hist_iter_flag:
-        #   print(cli_history[-hist_iter_flag], end='', flush=True)
-        #   _input_str = cli_history[-hist_iter_flag]
-        # else:
-        #   _lock_cli = True
-        #   hist_iter_flag -= 1
-
-      elif c == '\n':
+      if c == '\n':
         break
       _input_str += c
 
     cli_command = _input_str
     cli_history.append(cli_command)
 
-
-    # cli_command = input()
-    if _lock_cli:
-      logging.debug(f"{'='*30} {cli_command} {'='*30}")
 
     if cli_command == "stop":
       go = False
@@ -48,10 +32,17 @@ def main():
     elif cli_command == "debug on":
       _lock_cli = True
       logging.info('run "ets.debug_on()"')
-    elif cli_command == "hist":
+    elif cli_command[:4] == "hist":
       _lock_cli = True
       cli_history.pop()
-      logging.info(f'{cli_history = }')
+      _cli_commands = cli_command.split(' ')
+      if len(_cli_commands) == 1:
+        logging.info(f'{cli_history = }')
+      elif len(_cli_commands) == 2:
+        if len(cli_history) > int(_cli_commands[-1]):
+          cli_command = cli_history[int(_cli_commands[-1])]
+        else:
+          logging.error(f'Out of range. {cli_history = }')
 
     elif len(cli_command)>1 and cli_command[0]=='!':
       _lock_cli = True
@@ -60,22 +51,23 @@ def main():
           _cli_commands = cli_command[1:].split(' ')
           process_1 = subprocess.run(_cli_commands)
           logging.info(f'{process_1 = }')
-          logging.info(f'{process_1.args = }')
-          logging.info(f'{process_1.returncode = }')
-          logging.info(f'{process_1.stdout = }')
-        except Exception as e:
-          # logging.error("Not allowed.", exc_info=e)
+        except:
           logging.error("Not allowed.")
 
     elif len(cli_command)<1:
       _lock_cli = False
+      cli_history.pop()
       pass
 
     else:
       _lock_cli = True
+      cli_history.pop()
       logging.critical('FUNCTION NOT ALLOWED')
       logging.info("Typing 'stop' to terminate the server.")
-  
+    
+    if _lock_cli:
+      logging.debug(f"{'='*30} {cli_command} {'='*30}")
+
   logging.info('Goodbye!')
 
 
