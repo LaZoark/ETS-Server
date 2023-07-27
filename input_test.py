da1cf5eb8f9790f20d1216416ba4697ff6234d65
LaZoark <emersonchen086@icloud.com>
2023-07-27 15:19:24 +0800

    integral of CLI

--- input_test.py
+++ input_test.py
@@ -1,22 +1,8 @@
 from color_log import color
 logging = color.setup(name='Sniffer', level=color.DEBUG)
 
-
-import keyboard
-
 import sys
-
-# def on_key_release(key):
-#   if key == Key.right:
-#     print("Right key clicked")
-#   elif key == Key.left:
-#     print("Left key clicked")
-#   elif key == Key.up:
-#     print("Up key clicked")
-#   elif key == Key.down:
-#     print("Down key clicked")
-#   elif key == Key.esc:
-#     exit()
+import subprocess
 
 
 def main():
@@ -27,46 +13,66 @@ def main():
   logging.info("Typing 'stop' to terminate the server.")
 
   while go:
-    _input_str = ""
-    keyboard.add_abbreviation("@", "john@stackabuse.com")
+    _input_str: str = ""
     while True:
-      if keyboard.is_pressed('up'):
-        print("keyboard.is_pressed('up'):")
-        
-        keyboard.p
-        _lock_cli = False
-        hist_iter_flag += 1
-        if len(cli_history)>=hist_iter_flag:
-          print(cli_history[-hist_iter_flag], end='', flush=True)
-          _input_str = cli_history[-hist_iter_flag]
-        else:
-          _lock_cli = True
-          hist_iter_flag -= 1
-          # sys.stdout.flush()
-          # print('fffffffflush')
+      c = sys.stdin.read(1)
+      
+      if c == 'u':
+        pass
+        # _lock_cli = False
+        # hist_iter_flag += 1
+        # if len(cli_history)>=hist_iter_flag:
+        #   print(cli_history[-hist_iter_flag], end='', flush=True)
+        #   _input_str = cli_history[-hist_iter_flag]
+        # else:
+        #   _lock_cli = True
+        #   hist_iter_flag -= 1
 
-      c = sys.stdin.read(1) # reads one byte at a time, similar to getchar()
-
-      if c == '\n':
-        # sys.stdout.flush()
+      elif c == '\n':
         break
       _input_str += c
 
     cli_command = _input_str
     cli_history.append(cli_command)
 
+
     # cli_command = input()
     if _lock_cli:
       logging.debug(f"{'='*30} {cli_command} {'='*30}")
+
     if cli_command == "stop":
       go = False
     elif cli_command == "debug off":
+      _lock_cli = True
       logging.info('run "ets.debug_off()"')
     elif cli_command == "debug on":
+      _lock_cli = True
       logging.info('run "ets.debug_on()"')
     elif cli_command == "hist":
+      _lock_cli = True
+      cli_history.pop()
       logging.info(f'{cli_history = }')
+
+    elif len(cli_command)>1 and cli_command[0]=='!':
+      _lock_cli = True
+      if len(cli_command[1:])>1:
+        try:
+          _cli_commands = cli_command[1:].split(' ')
+          process_1 = subprocess.run(_cli_commands)
+          logging.info(f'{process_1 = }')
+          logging.info(f'{process_1.args = }')
+          logging.info(f'{process_1.returncode = }')
+          logging.info(f'{process_1.stdout = }')
+        except Exception as e:
+          # logging.error("Not allowed.", exc_info=e)
+          logging.error("Not allowed.")
+
+    elif len(cli_command)<1:
+      _lock_cli = False
+      pass
+
     else:
+      _lock_cli = True
       logging.critical('FUNCTION NOT ALLOWED')
       logging.info("Typing 'stop' to terminate the server.")
   
